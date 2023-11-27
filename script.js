c39110c0e5e06e1ba08510938a25a49ed608088b
DPachinik <151636730+DPachinik@users.noreply.github.com>
2023-11-27 10:47:57 -0300

    Tarea de operadores
    Mayor de Tres números

---
+++ script.js
@@ -0,0 +1,23 @@
+// primera propuesta//
+let num1=1;
+let num2=2;
+let num3=3;
+let Mayor;
+if(num1>num2 && num1>num3){
+   console.log("Mayor=num1");
+}else if(num2>num1 && num2>num3){
+    console.log("Mayor=num2");
+}else{
+    console.log("Mayor=num3");
+}
+// segunda propuesta//
+let NUM1=7;
+let NUM2=4;
+let NUM3=5;
+if(NUM1>NUM2 && NUM1>NUM3){
+   console.log("El mayor de todos es NUM1;");
+}else if(NUM2>NUM1 && NUM2>NUM3){
+    console.log("EL mayor de todos es NUM2");
+}else{
+    console.log("El mayor de todos es NUM3");
+}
